b29e62025f631187047dfe9a916f6b7bdc3d9f09
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2022-02-08 17:03:44 +0000

    unzipped the file

---
+++ Work Experience Unity Thing/Work Experience Unity Thing/Work_Experience/Assets/Scripts/Target.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float health = 50f;
+
+    public ParticleSystem hiteffect;
+    // this will be the effect that plays when the enemy is hit
+
+
+
+    public void TakeDamage(float amount)
+    {
+        hiteffect.Play();
+
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Work Experience Unity Thing/Work Experience Unity Thing/Work_Experience/Assets/Scripts/change_scene_trigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+// this is for scene management ^^
+
+
+public class change_scene_trigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int scene = 2;
+
+    // defined string here to allow me to change the scene number from unity
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+
+            // player will be a tag which i can make for only the player,
+            // this makes sure that the game does not think or allow any other object to trigger this event
+            
+        {
+            SceneManager.LoadScene(scene);
+
+            // i call the above over here (scene)
+
+            // this numbber is configured from the build settings
+            // 1 here is just the other scene, there will be multiple switches for this
+            // this is the level 1 one, it just is specific to the cube
+
+
+        }
+    }
+
+
+}
